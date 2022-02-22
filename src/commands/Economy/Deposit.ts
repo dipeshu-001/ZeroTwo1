547b2e861df629e37b14875843dbb5eee3a707c3
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2022-02-21 23:20:02 -0500

    Update Deposit.ts

--- src/commands/Economy/Deposit.ts
+++ src/commands/Economy/Deposit.ts
@@ -56,7 +56,7 @@ export default class Command extends BaseCommand {
     await this.client.deposit(user, amount);
     const buttonMessage: any = {
       contentText: `You have transferred *${amount} gold* to your bank.`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🚀CR-BOT🚀",
       buttons: buttons,
       headerType: 1,
     };
