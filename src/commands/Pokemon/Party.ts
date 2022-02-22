985f29f851d819d4e9e77bedc4088262a5c12062
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2022-02-22 00:16:51 -0500

    Update Party.ts

--- src/commands/Pokemon/Party.ts
+++ src/commands/Pokemon/Party.ts
@@ -50,7 +50,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🚀CR-BOT🚀",
       buttons: buttons,
       headerType: 1,
     };
