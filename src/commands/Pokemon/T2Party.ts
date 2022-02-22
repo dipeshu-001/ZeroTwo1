26848c3b00a08938953f3ff81534ed26c9c2f941
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2022-02-22 00:19:30 -0500

    Update T2Party.ts

--- src/commands/Pokemon/T2Party.ts
+++ src/commands/Pokemon/T2Party.ts
@@ -54,7 +54,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `You might want to check your pc.`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🚀CR-BOT🚀",
       buttons: buttons,
       headerType: 1,
     };
