7371f3dfe95a831ef515d5bfef8702c144dc4a5e
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-02-22 23:05:24 +0300

    Update Slot.ts

--- src/commands/Economy/Slot.ts
+++ src/commands/Economy/Slot.ts
@@ -104,7 +104,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${i}\n\n📉 You lost *${amount} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "🚀CR-BOT🚀",
+        footerText: "🚀DREADED-BOT🚀",
         buttons: buttons,
         headerType: 1,
       };
@@ -121,7 +121,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${o}\n\n📈 You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "🚀CR-BOT🚀",
+        footerText: "🚀DREADED-BOT🚀",
         buttons: buttons,
         headerType: 1,
       };
@@ -137,7 +137,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${p}\n\n🎊 *Jackpot!* You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "🚀CR-BOT🚀",
+        footerText: "🚀DREADED-BOT🚀",
         buttons: buttons,
         headerType: 1,
       };
