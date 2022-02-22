fb0d5d3b5d967bf3ab5536cf7beaf7d85e1b094b
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-02-22 23:01:22 +0300

    Update Gamble.ts

--- src/commands/Economy/Gamble.ts
+++ src/commands/Economy/Gamble.ts
@@ -114,7 +114,7 @@ export default class Command extends BaseCommand {
       await M.reply(await sticker.build(), MessageType.sticker, Mimetype.webp);
       const buttonMessage: any = {
         contentText: `📈 You won *${amount} gold*.`,
-        footerText: "🚀CR-BOT🚀",
+        footerText: "🚀DREADED-BOT🚀",
         buttons: buttons,
         headerType: 1,
       };
