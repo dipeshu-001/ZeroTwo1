cf2ff123298ee5ec1445af3a0e543d1ad067a8a0
Fortunatus Mokaya ☠️ <86984286+Fortunatusmokaya@users.noreply.github.com>
2022-02-22 23:42:53 +0300

    Update T2Pc.ts

--- src/commands/Pokemon/T2Pc.ts
+++ src/commands/Pokemon/T2Pc.ts
@@ -53,7 +53,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `You might want to check your party.`,
-      footerText: "🚀CR-BOT🚀",
+      footerText: "🚀DREADED-BOT🚀",
       buttons: buttons,
       headerType: 1,
     };
