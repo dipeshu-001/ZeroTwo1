dc24fb09c626f771ae5320e2b27a0013ceee24a0
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2022-02-22 00:17:33 -0500

    Update Pc.ts

--- src/commands/Pokemon/Pc.ts
+++ src/commands/Pokemon/Pc.ts
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🚀CR-BOT🚀",
       buttons: buttons,
       headerType: 1,
     };
