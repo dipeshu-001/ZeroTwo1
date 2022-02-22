92989d7824b989e31cc77130b854223b40d05d17
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2022-02-21 23:36:37 -0500

    Update support.ts

--- src/commands/General/support.ts
+++ src/commands/General/support.ts
@@ -19,12 +19,12 @@ export default class Command extends BaseCommand {
   run = async (M: ISimplifiedMessage): Promise<void> => {
     await this.client.sendMessage(
       M.sender.jid,
-      ` _*I'M FROM BEYOND 🎆*\n\n
-        _*BEYOND NEWS*_:https://chat.whatsapp.com/KcjW4C4Zl46L1ECpot1FeO\n\n 
-        _*WELCOME TO BEYOND*_:https://chat.whatsapp.com/HGH0SFq4w0B6IgSWJkgtt5\n\n
-        _*BEYOND CASINO*_:https://chat.whatsapp.com/EmfhxmgQhNfIUxiE8NHCvq\n\n 
-        _*BEYOND NSFW*_:https://chat.whatsapp.com/GSK6jAz783L107yJrjeV71
-        _*BEYOND QUIZ*_:https://chat.whatsapp.com/F6gqHxiHImD1vi5oc3ExBf`,
+      ` _*I'M FROM 𝕋ℍ𝔼 𝔻𝔸ℝ𝕂 🚀*\n\n
+        _*CR-BOT NEWS*_:https://chat.whatsapp.com/\n\n 
+        _*WELCOME TO THE DARK*_:https://chat.whatsapp.com/\n\n
+        _*CR-BOT CASINO*_:https://chat.whatsapp.com/\n\n 
+        _*CR-BOT NSFW*_:https://chat.whatsapp.com/
+        _*CR-BOT QUIZ*_:https://chat.whatsapp.com/`,
 
       MessageType.text
     );
