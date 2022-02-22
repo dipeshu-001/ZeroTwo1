d0d0dbb07bc63652b73b68d122078ae22651196a
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2022-02-22 00:15:56 -0500

    Update Catch.ts

--- src/commands/Pokemon/Catch.ts
+++ src/commands/Pokemon/Catch.ts
@@ -83,7 +83,7 @@ export default class Command extends BaseCommand {
           contentText: `Well done. You caught a level ${Level} ${this.client.util.capitalize(
             Name
           )}.`,
-          footerText: "🎇 Beyond 🎇",
+          footerText: "🚀CR-BOT🚀",
           buttons: buttons,
           headerType: 1,
         };
@@ -101,7 +101,7 @@ export default class Command extends BaseCommand {
           contentText: `Well done. You caught a level ${Level} ${this.client.util.capitalize(
             Name
           )}. It has been transferred to your pc.`,
-          footerText: "🎇 Beyond 🎇",
+          footerText: "🚀CR-BOT🚀",
           buttons: buttons,
           headerType: 1,
         };
