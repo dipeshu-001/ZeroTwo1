142322d8302927320a92ac58d0fe4d77659a3e5c
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2022-02-22 00:10:11 -0500

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -245,7 +245,7 @@ export default class Command extends BaseCommand {
       }
       const button: any = {
         buttonText: "Command List",
-        footerText: "🎇 Beyond 🎇",
+        footerText: "🚀CR-BOT🚀",
         description: `${text} 📝 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`,
         sections: sections,
         listType: 1,
