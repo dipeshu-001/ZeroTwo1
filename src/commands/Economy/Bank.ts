ee41cd87a6e71321b09eab1bdbe7f8dd50fe417e
Kakashig1112 <89956041+Kakashig1112@users.noreply.github.com>
2022-02-26 17:14:28 +0530

    Done

--- src/commands/Economy/Bank.ts
+++ src/commands/Economy/Bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "🚀DREADED-BOT🚀",
+      footerText: "©zero two 2022",
       buttons: buttons,
       headerType: 1,
     };
