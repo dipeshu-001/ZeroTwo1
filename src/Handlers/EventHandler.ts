08479c2eb8520cebfabe849f5fdb1f6423a00484
Kakashig1112 <89956041+Kakashig1112@users.noreply.github.com>
2022-02-26 16:55:13 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -53,7 +53,7 @@ export default class EventHandler {
       }
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "💎 Dreaded 💎",
+        footerText: "©zero two 2022",
         buttons: buttons,
         headerType: 1,
       };
