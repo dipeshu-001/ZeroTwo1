730ffa782cd7bf19b2cdad6b134b95d9d47da915
Kakashig1112 <89956041+Kakashig1112@users.noreply.github.com>
2022-03-13 18:40:40 +0530

    Levi

--- src/commands/Economy/Bank.ts
+++ src/commands/Economy/Bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "levi",
       buttons: buttons,
       headerType: 1,
     };
