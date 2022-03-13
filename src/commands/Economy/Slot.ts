90d09cb6395b6b1aff4841c363144423fc357620
Kakashig1112 <89956041+Kakashig1112@users.noreply.github.com>
2022-03-13 18:38:57 +0530

    Levi

--- src/commands/Economy/Slot.ts
+++ src/commands/Economy/Slot.ts
@@ -121,7 +121,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${o}\n\n🎊✨ You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "©zero two 2022",
+        footerText: "©levi 2022",
         buttons: buttons,
         headerType: 1,
       };
@@ -137,7 +137,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${p}\n\n🎊 *Jackpot!* You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "©zero two 2022",
+        footerText: "©levi 2022",
         buttons: buttons,
         headerType: 1,
       };
