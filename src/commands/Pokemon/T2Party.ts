467c78ae361eb16e55a4ec4b849f9f290687f897
Kakashig1112 <89956041+Kakashig1112@users.noreply.github.com>
2022-03-18 02:46:50 +0530

    Update T2Party.ts

--- src/commands/Pokemon/T2Party.ts
+++ src/commands/Pokemon/T2Party.ts
@@ -54,7 +54,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `You might want to check your pc.`,
-      footerText: "🚀DREADED-BOT🚀",
+      footerText: "🚀LEVI",
       buttons: buttons,
       headerType: 1,
     };
