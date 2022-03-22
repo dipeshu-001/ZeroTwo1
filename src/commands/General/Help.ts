0e4c60e87071a1190b91d9765d7ce8b902340c7f
Lakshsharma31 <100475528+Lakshsharma31@users.noreply.github.com>
2022-03-22 21:06:27 +0530

    Dynamite

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
      run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
            const n = [
-           'https://telegra.ph/file/4788080301788f47fe799.mp4;'
+           'https://www.linkpicture.com/q/3538991.png;'
         ]
         let chitoge = n[Math.floor(Math.random() * n.length)]
         if (!parsedArgs.joined) {
