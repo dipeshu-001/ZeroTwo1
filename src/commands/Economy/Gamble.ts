9991f4202f29d4194e6280f133fa27c3172a286c
Kakashig1112 <89956041+Kakashig1112@users.noreply.github.com>
2022-03-22 12:49:50 +0530

    Update Gamble.ts

--- src/commands/Economy/Gamble.ts
+++ src/commands/Economy/Gamble.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
     { joined }: IParsedArgs
   ): Promise<void> => {
     /*eslint-disable @typescript-eslint/no-explicit-any*/
-    if (M.from !== "120363038329584084@g.us")
+    if (M.from !== "120363025090167429@g.us")
       return void M.reply(
         `You can't gamble here. Use ${this.client.config.prefix}support to get casino group link.`
       );
