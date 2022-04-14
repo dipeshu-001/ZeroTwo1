f72bab961dc6c098bd330836a3442fbb81f16c29
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-14 09:39:19 +0530

    Update Wallet.ts

--- src/commands/Economy/Wallet.ts
+++ src/commands/Economy/Wallet.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `👛 *Wallet | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "levi",
+      footerText: "ZeroTwo 2022",
       buttons: buttons,
       headerType: 1,
     };
