c4b24bc8119460245f7ba3c90eef52aae911f69c
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-14 09:38:27 +0530

    Update Slot.ts

--- src/commands/Economy/Slot.ts
+++ src/commands/Economy/Slot.ts
@@ -104,7 +104,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${i}\n\n📉 You lost *${amount} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "©zero two 2022",
+        footerText: "ZeroTwo 2022",
         buttons: buttons,
         headerType: 1,
       };
@@ -121,7 +121,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${o}\n\n🎊✨ You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "©levi 2022",
+        footerText: "ZeroTwo 2022",
         buttons: buttons,
         headerType: 1,
       };
@@ -137,7 +137,7 @@ export default class Command extends BaseCommand {
       const text = `${head}\n\n${p}\n\n🎊 *Jackpot!* You won *${gold} gold*.`;
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "©levi 2022",
+        footerText: "ZeroTwo 2022",
         buttons: buttons,
         headerType: 1,
       };
