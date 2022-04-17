eba9a22bbdfa1ea748494a2a6b658922c0609856
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-17 08:45:14 +0530

    Update WAClient.ts

--- src/lib/WAClient.ts
+++ src/lib/WAClient.ts
@@ -361,7 +361,7 @@ export default class WAClient extends Base {
     ];
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "✞︎☠︎︎Elsina✞︎☠︎︎",
+      footerText: "© ZeroTwo 2022",
       buttons: buttons,
       headerType: 4,
       imageMessage: media?.message?.imageMessage,
@@ -405,7 +405,7 @@ export default class WAClient extends Base {
     ];
     const buttonMessage: any = {
       contentText: `*A claimable character Appeared!*\n\n🎀 *Name: ${chara.name}*\n\n💬 *About:* ${chara.about}\n\n📛 *Source: ${source[0].anime.title}*\n\n💰 *Price: ${price}*\n\n*[Use ${this.config.prefix}claim to have this character in your gallery]*`,
-      footerText: "✞︎☠︎︎ Elaina ✞︎☠︎︎",
+      footerText: "©ZeroTwo 2022",
       buttons: buttons,
       headerType: 4,
       imageMessage: media?.message?.imageMessage,
