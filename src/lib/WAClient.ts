f5e7a46ee67e81dec57402693539a0110dfd1123
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-19 11:13:41 +0530

    Update WAClient.ts

--- src/lib/WAClient.ts
+++ src/lib/WAClient.ts
@@ -361,7 +361,7 @@ export default class WAClient extends Base {
     ];
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "✞︎☠︎︎Elsina✞︎☠︎︎",
+      footerText: "©ZeroTwo 2022",
       buttons: buttons,
       headerType: 4,
       imageMessage: media?.message?.imageMessage,
