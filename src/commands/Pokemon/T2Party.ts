bd149e46c38820287be667f3f2331d15f7d270ac
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-19 13:17:08 +0530

    Update T2Party.ts

--- src/commands/Pokemon/T2Party.ts
+++ src/commands/Pokemon/T2Party.ts
@@ -54,7 +54,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `You might want to check your pc.`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🍭ZeroTwo 🍭",
       buttons: buttons,
       headerType: 1,
     };
