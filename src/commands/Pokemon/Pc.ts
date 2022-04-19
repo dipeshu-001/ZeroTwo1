f303378f9cf49d5b2cf8ef456eac042496b3c2c8
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-19 13:16:39 +0530

    Update Pc.ts

--- src/commands/Pokemon/Pc.ts
+++ src/commands/Pokemon/Pc.ts
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🍭ZeroTwo🍭",
       buttons: buttons,
       headerType: 1,
     };
