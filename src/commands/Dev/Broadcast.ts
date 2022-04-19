c62c4b8b36998bb84d56127dc1b77a0d1808bdf1
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-19 13:25:39 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -1,29 +1,54 @@
-import { MessageType } from '@adiwajshing/baileys'
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import {  IParsedArgs, ISimplifiedMessage } from '../../typings'
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'broadcast',
-            description: 'Sends msg to all group chats',
-            aliases: ['BC', 'announcement','bc'],
-            category: 'dev',
-            usage: `${client.config.prefix}broadcast`,
-            modsOnly: true,
-            baseXp: 0
-        })
-    }
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "broadcast",
+			description:
+				"Will make a broadcast for groups where the bot is in. Can be used to make announcements.",
+			aliases: ["bcast", "announcement", "bc"],
+			category: "dev",
+			dm: true,
+			usage: `${client.config.prefix}bc`,
+			modsOnly: true,
+			baseXp: 0,
+		});
+	}
 
-    run = async (M: ISimplifiedMessage,  { joined }: IParsedArgs): Promise<void> => {
-        
-        const term = joined.trim();
-        const chats:any= this.client.chats.all().filter(v => !v.read_only && !v.archive).map(v => v.jid).map(jids => jids.includes("g.us")? jids : null).filter(v=>v);
-        for(let i =0;i<chats.length;i++){
-        const text = `*🤍✨ Levi Broadcast🤍✨ * \n 📢  \n${term} \n *Regards:* ${M.sender.username}`
-        this.client.sendMessage(chats[i], text,MessageType.text,{contextInfo : {mentionedJid : M.groupMetadata?.participants.map((user) => user.jid) }})
-        }
-    }
+	run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+		if (!joined)
+			return void (await M.reply(`Please provide the Broadcast Message.`));
+		const term = joined.trim();
+		const gifs = [
+			"https://telegra.ph/file/051fcf17c7e1ed288c79a.mp4",
+		];
+		const selected = gifs[Math.floor(Math.random() * gifs.length)];
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const chats: any = this.client.chats
+			.all()
+			.filter((v) => !v.read_only && !v.archive)
+			.map((v) => v.jid)
+			.map((jids) => (jids.includes("g.us") ? jids : null))
+			.filter((v) => v);
+		for (let i = 0; i < chats.length; i++) {
+			const text = `*🍭「ZeroTwo Broadcast」🍭*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
+				mimetype: Mimetype.gif,
+				caption: `${text}`,
+				contextInfo: {
+					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
+				},
+			});
+		}
+		await M.reply(`✅ Broadcast Message sent to *${chats.length} groups*.`);
+	};
 }
