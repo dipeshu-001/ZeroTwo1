ddee84b6e8f4e49fa56ab041cc1baaa3f2f64aae
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-04-22 10:18:32 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://telegra.ph/file/90c8d596818e948cc6a82.mp4'
+            'https://telegra.ph/file/90c8d596818e948cc6a82.mp4','https://telegra.ph/file/ff95de880cf258150fe84.mp4','https://telegra.ph/file/3bb24cecc8094ff80e928.mp4'
         ]
         let well = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
