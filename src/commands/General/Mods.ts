b74c948ba144fb7ebd144b1c9919654574898caf
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-06-04 12:48:23 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -1,33 +1,29 @@
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { ISimplifiedMessage } from "../../typings";
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
-  constructor(client: WAClient, handler: MessageHandler) {
-    super(client, handler, {
-      command: "mods",
-      description: "Displays the Moderators' contact info",
-      category: "general",
-      usage: `${client.config.prefix}mods`,
-      aliases: ["moderators", "mod", "owner"],
-    });
-  }
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'mods',
+            description: "Displays the Moderators' contact info",
+            category: 'general',
+            usage: `${client.config.prefix}mods`,
+            aliases: ['moderators', 'mod', 'owner']
+        })
+    }
 
-  run = async (M: ISimplifiedMessage): Promise<void> => {
-    if (!this.client.config.mods || !this.client.config.mods[0])
-      return void M.reply("*[UNMODERATED]*");
-    const filteredMap = this.client.config.mods
-      .map((mod) => this.client.getContact(mod))
-      .filter((user) => user);
-    let text = "🧧 Moderators 🧧\n\n";
-    filteredMap.forEach(
-      (user, index) =>
-        (text += `#${index + 1}\n🍂 *Username: ${
-          user.notify || user.vname || user.name || "null"
-        }*\n🍁 *Contact: https://wa.me/+${user?.jid?.split("@")[0]}*\n\n`)
-    );
-    text += `\n 𝐙𝐞𝐫𝐨𝐓𝐰𝐨`;
-    return void M.reply(text);
-  };
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
+        const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
+        let text = '🍭 𝐎𝐰𝐧𝐞𝐫 𝐨𝐟 𝐙𝐞𝐫𝐨𝐓𝐰𝐨 🍭\n\n'
+        filteredMap.forEach(
+            (user, index) =>
+                (text += `#${index + 1}\n🍀 *Username: ${
+                    user.notify || user.vname || user.name || 'null'
+                }*\n🧧 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+        )
+        return void M.reply(text)
+    }
 }
