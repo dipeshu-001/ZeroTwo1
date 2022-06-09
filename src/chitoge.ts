962269709df890af1a551fb5284461dbb892c2ca
İşşa <Issamoha020@gmail.com>
2022-06-09 17:05:04 +0300

    Update chitoge.ts

--- src/chitoge.ts
+++ src/chitoge.ts
@@ -15,8 +15,8 @@ import NewsHandler from "./Handlers/NewsHandler";
 
 if (!process.env.MONGO_URI) throw new Error("MONGO URL IS NOT PROVIDED");
 const client = new WAClient({
-  name: process.env.NAME || "Asuna",
-  session: process.env.SESSION || "Asuna",
+  name: process.env.NAME || "ZeroTwo1",
+  session: process.env.SESSION || "ZeroTwo1",
   prefix: process.env.PREFIX || ":",
   mods: (process.env.MODS || "")
     .split(", ")
